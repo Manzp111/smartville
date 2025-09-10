09db9cae8e5eda1e2b5443d94110a6e489e8e28a
kwizera <kwizerad200@gmail.com>
2025-09-10 21:10:05 +0200

    creating event endpoints

--- event/views.py
+++ event/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Event
+from .serializers import EventSerializer
 
-# Create your views here.
+
+class EventViewSet(viewsets.ModelViewSet):
+    queryset = Event.objects.all().order_by("-created_at")
+    serializer_class = EventSerializer
+    #permission_classes = [permissions.IsAuthenticated]
+    permission_classes = [permissions.AllowAny]
+
+    def perform_create(self, serializer):
+        #serializer.save(organizer=self.request.user)  # put login first
+        serializer.save()
