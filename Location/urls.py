a7ead316b5c027dfb1d7fc072a7329a525dac2be
Manzp111 <gilbertnshimyimana11@gmail>
2025-09-11 23:37:54 +0200

    update packages

--- Location/urls.py
+++ Location/urls.py
@@ -1,7 +1,15 @@
 from django.urls import path
 from . import views
+from django.urls import path, include
 from .locationviews import LocatePointAPIView
+from rest_framework.routers import DefaultRouter
+
+
+router = DefaultRouter()
+
+
 urlpatterns = [
     path("locate/", views.locate_point, name="locate_village"),
-    path("api/locate/", LocatePointAPIView.as_view(), name="locate_point_api"),
+    # path("api/locate/", LocatePointAPIView.as_view(), name="locate_point_api"),
+  
 ]
