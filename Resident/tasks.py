a5c9f58e2096aa2824f41a618ae702fe082da373
Manzp111 <gilbertnshimyimana11@gmail>
2025-09-12 13:00:28 +0200

    joining village

--- Resident/tasks.py
+++ Resident/tasks.py
@@ -4,7 +4,7 @@ from django.core.mail import send_mail
 from django.conf import settings
 
 @shared_task
-def notify_village_leader_new_resident(leader_email, resident_name, village_name):
-    subject = f"New Resident Joined Your Village ({village_name})"
+def notify_village_leader_new_resident(leader_email, resident_name, village_name,location):
+    subject = f"New Resident Joined Your Village ({village_name}located on {location})"
     message = f"{resident_name} has joined your village {village_name}."
     send_mail(subject, message, settings.DEFAULT_FROM_EMAIL, [leader_email])
