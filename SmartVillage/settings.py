bc6712002b5d342a9805572e66519155ee3ba916
Manzp111 <gilbertnshimyimana11@gmail>
2025-09-16 08:44:45 +0200

    Fix line endings for start.sh

--- SmartVillage/settings.py
+++ SmartVillage/settings.py
@@ -157,7 +157,7 @@ EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 
 # CELERY (Redis)
 CELERY_BROKER_URL = config('REDIS_URL', default='redis://localhost:6379/0')
-CELERY_RESULT_BACKEND = config('REDIS_URL', default='redis://localhost:6379/0')
+CELERY_RESULT_BACKEND = config('REDIS_URL', default='redis://localhost:6379/1')
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 CELERY_RESULT_SERIALIZER = "json"
