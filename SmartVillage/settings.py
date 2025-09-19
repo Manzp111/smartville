75f311c148c219357f03962c04ebe4a7ed276b1e
Nshimyimana Gilbert <149456634+Manzp111@users.noreply.github.com>
2025-09-19 16:18:32 +0200

    Update settings.py

--- SmartVillage/settings.py
+++ SmartVillage/settings.py
@@ -131,7 +131,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "ACCESS_TOKEN_LIFETIME": timedelta(seconds=10),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
     "ROTATE_REFRESH_TOKENS": True,
     "BLACKLIST_AFTER_ROTATION": True,
@@ -176,3 +176,4 @@ CORS_ALLOWED_ORIGINS_ALL=True
 # MEDIA
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
