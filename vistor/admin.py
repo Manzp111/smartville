ba28b8f3de42101975609059bfa6b680a8df9d53
Manzp111 <gilbertnshimyimana11@gmail>
2025-09-22 11:13:27 +0200

    adding vistor to admin

--- vistor/admin.py
+++ vistor/admin.py
@@ -1,26 +1,21 @@
-# from django.contrib import admin
-# from .models import Visitor
+from django.contrib import admin
+from .models import Visitor
 
-# @admin.register(Visitor)
-# class VisitorAdmin(admin.ModelAdmin):
-#     list_display = (
-#         "visitor_id",
-#         "visitor_info",
-#         "host",
-#         "visitor_location",
-#         "reason_for_visit",
-#         "arrival_date",
-#         "departure_date",
-#         "registered_date",
-#         "updated_at",
-#     )
-#     list_filter = ("arrival_date", "departure_date", "visitor_location", "host")
-#     search_fields = ("visitor_info__first_name", "visitor_info__last_name", "host__email", "reason_for_visit")
-#     ordering = ("-registered_date",)
-#     actions = ["delete_selected"]  # default bulk delete action
-
-#     # Optional: customize display for visitor info
-#     def visitor_info(self, obj):
-#         return f"{obj.visitor_info.first_name} {obj.visitor_info.last_name}"
-
-#     visitor_info.short_description = "Visitor Name"
+@admin.register(Visitor)
+class VisitorAdmin(admin.ModelAdmin):
+    list_display = (
+        "visitor_id",
+        "name",
+        "resident",
+        "phone_number",
+        "purpose_of_visit",
+        "date",
+        "time_in",
+        "time_out",
+        "village",
+        "created_at",
+    )
+    list_filter = ("date", "village", "resident")
+    search_fields = ("name", "phone_number", "id_number", "resident__name")
+    ordering = ("-created_at",)
+    readonly_fields = ("visitor_id", "created_at", "updated_at")
